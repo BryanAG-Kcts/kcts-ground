dc4ce7564164f8be2f2ab6b39d83f396512dca10
Bryan David Alvarez Galvis <bryanalvarezg.kcts@gmail.com>
2024-02-10 08:16:04 -0500

    download

--- src/components/config/download.tsx
+++ src/components/config/download.tsx
@@ -9,25 +9,12 @@ interface Props {
   js?: string
 }
 export const Download = ({ html = '', css = '', js = '' }: Props): JSX.Element => {
-  async function download (): Promise<void> {
-    const htmlToCode = generateIframeHTML(atob(html), atob(css), atob(js))
-    const code = btoa(htmlToCode)
-    try {
-      const data = await fetch(`/${code}`)
-      const blob = await data.blob()
-      const url = URL.createObjectURL(blob)
-      const a = document.createElement('a')
-      a.href = url
-      a.download = 'code.pdf'
-      a.click()
-    } catch (e) {
-      alert('Lo lamentamos, algo')
-    }
-  }
+  const htmlToCode = generateIframeHTML(atob(html), atob(css), atob(js))
+  const code = btoa(htmlToCode)
 
   return (
-    <button onClick={download}>
+    <a href={`/${code}`} download>
       <ReactSVG src='/download.svg' title='Descargar pdf' desc='Descargar pdf' />
-    </button>
+    </a>
   )
 }
