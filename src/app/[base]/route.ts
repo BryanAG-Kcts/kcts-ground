e63b5b955536ac7601f2d70edccb36da367c94d9
Bryan David Alvarez Galvis <bryanalvarezg.kcts@gmail.com>
2024-02-10 09:05:24 -0500

    catch error

--- src/app/[base]/route.ts
+++ src/app/[base]/route.ts
@@ -8,25 +8,29 @@ interface Props {
 }
 
 export async function GET (_: NextRequest, { params }: Props): Promise<NextResponse> {
-  const code = atob(params.base)
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
+  try {
+    const code = atob(params.base)
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
 
-  const pr1 = page.setContent(code, { waitUntil: 'domcontentloaded' })
-  const pr2 = page.emulateMediaType('screen')
+    const pr1 = page.setContent(code, { waitUntil: 'domcontentloaded' })
+    const pr2 = page.emulateMediaType('screen')
 
-  await Promise.all([pr1, pr2])
+    await Promise.all([pr1, pr2])
 
-  const pdf = await page.pdf({
-    margin: { top: '0', right: '0', bottom: '0', left: '0' },
-    printBackground: true,
-    format: 'A4'
-  })
+    const pdf = await page.pdf({
+      margin: { top: '0', right: '0', bottom: '0', left: '0' },
+      printBackground: true,
+      format: 'A4'
+    })
 
-  await browser.close()
+    await browser.close()
 
-  const headers = new Headers()
-  headers.set('Content-Type', 'application/pdf')
-  headers.set('Content-Disposition', 'attachment; filename=code.pdf')
-  return new NextResponse(pdf, { headers })
+    const headers = new Headers()
+    headers.set('Content-Type', 'application/pdf')
+    headers.set('Content-Disposition', 'attachment; filename=code.pdf')
+    return new NextResponse(pdf, { headers })
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message })
+  }
 }
