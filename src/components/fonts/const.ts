69e786120c54b0bc6229e4468b93b101fe977a58
Kactuswow <bryanalvarezg.kcts@gmail.com>
2024-02-27 09:33:47 -0500

    Protocolo https

--- src/components/fonts/const.ts
+++ src/components/fonts/const.ts
@@ -1,7 +1,8 @@
 export function getFont (file: string, name: string): string {
+  const url = file.replace('http', 'https')
   return `@font-face {
     font-family: '${name}';
-    src: url('${file}') format('truetype');
+    src: url('${url}') format('truetype');
 }
     `
 }
